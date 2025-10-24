ab405fd7ac87c12dc7fa03feffbc13c54e28f635
René Backhaus <rene.backhaus@real-digital.de>
2025-10-24 20:21:51 +0200

    format code

--- tokenizer/raw_tokenizer.c
+++ tokenizer/raw_tokenizer.c
@@ -4,16 +4,17 @@
 #include "raw_tokenizer.h"
 
 int is_space(char c);
+
 int is_raw_operator(char c);
-RawToken* create_new_raw_token();
+
+RawToken *create_new_raw_token();
+
 void push_raw_token_to_list(RawTokenList *list, RawToken token);
 
-RawTokenList tokenizer(const char query[], size_t query_length)
-{
+RawTokenList tokenizer(const char query[], size_t query_length) {
     RawTokenList *raw_token_list = malloc(sizeof(RawTokenList));
 
-    if (raw_token_list == NULL)
-    {
+    if (raw_token_list == NULL) {
         perror("Failed to allocate memory for raw token list!");
         exit(-1);
     }
@@ -32,12 +33,10 @@ RawTokenList tokenizer(const char query[], size_t query_length)
 
     int i = 0;
 
-    while (query[i] != '\0')
-    {
+    while (query[i] != '\0') {
         char current_char = query[i];
 
-        if (is_space(current_char))
-        {
+        if (is_space(current_char)) {
             if (raw_token->length > 0) {
                 raw_token->value[raw_token->length] = '\0';
                 push_raw_token_to_list(raw_token_list, *raw_token);
@@ -49,8 +48,7 @@ RawTokenList tokenizer(const char query[], size_t query_length)
             continue;
         }
 
-        if (is_raw_operator(current_char))
-        {
+        if (is_raw_operator(current_char)) {
             // previous token e.g "name,"
             // store "name"
             if (raw_token->length > 0) {
@@ -84,9 +82,9 @@ RawTokenList tokenizer(const char query[], size_t query_length)
 
     return *raw_token_list;
 }
+
 void push_raw_token_to_list(RawTokenList *list, RawToken token) {
-    if (list->size >= list->capacity)
-    {
+    if (list->size >= list->capacity) {
         list->capacity *= 2;
         list->tokens = realloc(list->tokens, list->capacity * sizeof(RawToken));
 
@@ -104,11 +102,11 @@ void push_raw_token_to_list(RawTokenList *list, RawToken token) {
 
     list->tokens[list->size++] = copy_token;
 }
-RawToken* create_new_raw_token() {
+
+RawToken *create_new_raw_token() {
     RawToken *raw_token = malloc(sizeof(RawToken));
 
-    if (raw_token == NULL)
-    {
+    if (raw_token == NULL) {
         perror("Failed to allocate memory for raw token!");
         exit(-1);
     }
@@ -116,8 +114,7 @@ RawToken* create_new_raw_token() {
     raw_token->length = 0;
     raw_token->value = malloc(raw_token->length + 1);
 
-    if (raw_token->value == NULL)
-    {
+    if (raw_token->value == NULL) {
         perror("Failed to allocate memory for raw token value!");
         free(raw_token);
         exit(-1);
@@ -126,18 +123,15 @@ RawToken* create_new_raw_token() {
     return raw_token;
 }
 
-int is_space(const char c)
-{
+int is_space(const char c) {
     return c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r';
 }
 
-int is_raw_operator(const char c)
-{
+int is_raw_operator(const char c) {
     return c == '>' || c == '<' || c == '=' || c == '(' || c == ')' || c == ',' || c == ';';
 }
 
-int main()
-{
+int main() {
     char *data_provider[] = {
         "SELECT name, age FROM wombats WHERE age > 3;",
         "SELECT name,age FROM wombats WHERE age > 4;",
@@ -148,19 +142,17 @@ int main()
         "SELECT name, age FROM wombats WHERE age > 7 ORDER BY age ASC LIMIT 1 OFFSET 2;",
         "INSERT INTO table_name (column1, column2, column3) VALUES (value1, value2, value3);",
         "CREATE TABLE table_name (column1, column2, column3);"
-        };
+    };
 
     int data_provider_count = sizeof(data_provider) / sizeof(data_provider[0]);
 
-    for (int i = 0; i < data_provider_count; i++)
-    {
+    for (int i = 0; i < data_provider_count; i++) {
         char query[256];
         strcpy(query, data_provider[i]);
 
         //printf("RAW TOKEN: %d\n", i);
         RawTokenList result = tokenizer(query, strlen(query) + 1);
-        for (int j = 0; j < result.size; j++)
-        {
+        for (int j = 0; j < result.size; j++) {
             printf("VALUE %d -> %s || ", j, result.tokens[j].value);
         }
         printf("\n");
